dd6e253261d4b9a5bce245c41fbb9637bf1d96fa
Angelov <nangelov@hpe.com>
2026-03-31 23:41:44 +0300

    Add nav item for addresses with MapPinIcon to account sidebar

--- components/AccountNav.tsx
+++ components/AccountNav.tsx
@@ -38,6 +38,15 @@ function PencilIcon() {
   );
 }
 
+function MapPinIcon() {
+  return (
+    <svg {...iconProps}>
+      <path d="M20 10c0 6-8 12-8 12s-8-6-8-12a8 8 0 0 1 16 0Z" />
+      <circle cx="12" cy="10" r="3" />
+    </svg>
+  );
+}
+
 function ShieldIcon() {
   return (
     <svg {...iconProps}>
@@ -90,6 +99,7 @@ const sections: NavSection[] = [
     items: [
       { href: '/account', label: 'Профил', icon: <UserIcon /> },
       { href: '/account/edit', label: 'Редактиране', icon: <PencilIcon /> },
+      { href: '/account/addresses', label: 'Адреси', icon: <MapPinIcon /> },
       { href: '/account/security', label: 'Сигурност', icon: <ShieldIcon /> },
     ],
   },
